e7ad614f5cba121247fb8d456f6c702c7d6418ef
bashar <bashar03@icloud.com>
2024-05-13 23:26:51 +0300

    draft

---
+++ app/(profile)/index.tsx
@@ -0,0 +1,27 @@
+import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import ProfileHeader from '@/components/profile/profileHeader'
+import { VStack } from 'native-base'
+
+const Profile = () => {
+    const [tabValue, setTabValue] = useState<number>(0)
+    // const X = () => {
+    //     switch (tabValue) {
+    //         case 0:
+    //             return <EventComponent /> // return the component
+    //         case 1:
+    //             return <PublicPlace />
+    //         default:
+    //             <EventComponent />
+    //     }
+    // }
+    return (
+        <VStack space={2}>
+            {/* <ProfileHeader />
+            <Tabs value={tabValue} setValue={(val: number) => setTabValue(val)} /> 
+            {X} */}
+        </VStack>
+    )
+}
+
+export default Profile
--- app/rootLayoutNav.tsx
+++ app/rootLayoutNav.tsx
@@ -1,38 +1,36 @@
 import React from 'react'
 import { Stack } from 'expo-router'
 import { NativeBaseProvider, extendTheme } from 'native-base'
-import { Colors } from './theme'
-import { useColorScheme } from 'react-native'
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
-
-export const unstable_settings = {
+// import { Colors } from './theme'
+// import { useColorScheme } from 'react-native'
+// import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
+ export const unstable_settings = {
     initialRouteName: "(tabs)/index",
 };
 
 const RootLayoutNav = () => {
-    const colorScheme = useColorScheme()
-    const nativeBaseTheme = extendTheme({
-        colors: Colors[colorScheme ?? 'light'],
-        fonts: {
-            urbanist: 'Urbanist-Regular',
-        },
-        config: {
-            initialColorMode: colorScheme ?? 'light'
-        }
-    });
+    // const colorScheme = useColorScheme()
+    // const nativeBaseTheme = extendTheme({ kib
+    //     colors: Colors[colorScheme ?? 'light'],
+    //     fonts: {
+    //         urbanist: 'Urbanist-Regular',
+    //     },
+    //     config: {
+    //         initialColorMode: colorScheme ?? 'light'
+    //     }
+    // });
 
+    // <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
     return (
-        <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-            <NativeBaseProvider theme={nativeBaseTheme}>
-                <Stack screenOptions={{ headerShown: false }}>
-                    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                    <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-                    <Stack.Screen name="(notifications)" options={{ headerShown: false }} />
-                    <Stack.Screen name="(details)" options={{ headerShown: false }} />
-                    <Stack.Screen name="modal" options={{ presentation: "modal" }} />
-                </Stack>
-            </NativeBaseProvider>
-        </ThemeProvider>
+        <NativeBaseProvider>
+            <Stack screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+                <Stack.Screen name="(notifications)" options={{ headerShown: false }} />
+                <Stack.Screen name="(details)" options={{ headerShown: false }} />
+                <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+            </Stack>
+        </NativeBaseProvider>
     )
 }
 
